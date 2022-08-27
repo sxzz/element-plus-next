0f8c422637a34b48e9fbf715549b3a60e9c1d56e
三咲智子 <sxzz@sxzz.moe>
2022-08-28 04:06:22 +0800

    feat(vue-component): sync icon

---
+++ packages/web/vue-components/src/icon/src/icon.ts
@@ -0,0 +1,14 @@
+import { buildProps, definePropType } from '@element-plus-next/vue-utils'
+import type { ExtractPropTypes } from 'vue'
+import type Icon from './icon.vue'
+
+export const iconProps = buildProps({
+  size: {
+    type: definePropType<number | string>([Number, String]),
+  },
+  color: {
+    type: String,
+  },
+} as const)
+export type IconProps = ExtractPropTypes<typeof iconProps>
+export type IconInstance = InstanceType<typeof Icon>
---
+++ packages/web/vue-components/src/icon/src/icon.vue
@@ -0,0 +1,29 @@
+<template>
+  <i :class="ns.b()" :style="style" v-bind="$attrs">
+    <slot />
+  </i>
+</template>
+
+<script lang="ts" setup>
+import { computed } from 'vue'
+import { addUnit, isUndefined } from '@element-plus-next/utils'
+import { useNamespace } from '@element-plus-next/vue-hooks'
+import { iconProps } from './icon'
+import type { CSSProperties } from 'vue'
+
+defineOptions({
+  name: 'ElIcon',
+  inheritAttrs: false,
+})
+const props = defineProps(iconProps)
+const ns = useNamespace('icon')
+
+const style = computed<CSSProperties>(() => {
+  if (!props.size && !props.color) return {}
+
+  return {
+    fontSize: isUndefined(props.size) ? undefined : addUnit(props.size),
+    '--color': props.color,
+  }
+})
+</script>
