990e54c5233de7d7ee769b47e8289fc1ff1376df
三咲智子 <sxzz@sxzz.moe>
2022-08-28 03:39:31 +0800

    feat(vue-directives): sync

---
+++ packages/web/vue-directives/src/repeat-click/index.ts
@@ -0,0 +1,34 @@
+import type { DirectiveBinding, ObjectDirective } from 'vue'
+
+const RepeatClick: ObjectDirective = {
+  beforeMount(el: HTMLElement, binding: DirectiveBinding) {
+    let interval: ReturnType<typeof setInterval> | null = null
+    let isHandlerCalled = false
+
+    const handler = () => binding.value && binding.value()
+
+    const clear = () => {
+      clearInterval(interval!)
+      interval = null
+
+      if (!isHandlerCalled) {
+        handler()
+      }
+      isHandlerCalled = false
+    }
+
+    el.addEventListener('mousedown', (e: MouseEvent) => {
+      if (e.button !== 0) return
+
+      document.addEventListener('mouseup', clear, { once: true })
+
+      clearInterval(interval!)
+      interval = setInterval(() => {
+        isHandlerCalled = true
+        handler()
+      }, 100)
+    })
+  },
+}
+
+export default RepeatClick
